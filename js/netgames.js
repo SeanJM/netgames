efead00c03805396f80adaad9d6118390892203c
SeanJM <seanjmacisaac@gmail.com>
2012-12-11 10:17:18 -0500

    Initial header coding is done

--- js/netgames.js
+++ js/netgames.js
@@ -1,10 +1,64 @@
 $(function() {
   var signIn = $('#status .sign-in');
-  signIn.find('.guest').click(function(){
-    $(signIn).find('.user-popup').addClass('visible');
+  signIn.find('.guest').click(function(event){
+    signIn.find('.user-popup').toggleClass('visible');
+    signIn.find('.user-popup .login').addClass('visible');
+    event.stopPropagation();
+  });
+
+  var account = $('#status .account');
+  account.find('.user').on('click',function(event){
+    account.find('.user-popup').toggleClass('visible');
+    account.find('.user-popup .login').addClass('visible');
+    event.stopPropagation();
+  });
+  account.find('.logout').closest('a').on('click',function(){
+    $('#status').removeClass('logged-in');
+  });
+
+  var cart = $('#status .cart')
+  $('#open-cart').click(function(event){
+    cart.find('.cart-popup').toggleClass('visible');
+    event.stopPropagation();
+  });
+
+  var wishlist = $('#status .wishlist')
+  $('#open-wishlist').click(function(event){
+    wishlist.find('.popup').toggleClass('visible');
+    event.stopPropagation();
   });
 
   $('input').each(function(){
     $(this).placeholder();
+  });
+  
+  $('.visible').on('click',function(event){
+    event.stopPropagation();
+  });
+
+  $('html').on('click',function(){
+    $('.visible').removeClass('visible');
+  });
+
+  $('.switch .btn').on('click',function(event){
+    $(this).closest('.switch').find('.active').removeClass('active');
+    $(this).addClass('active');
+    event.stopPropagation();
+  });
+
+  $('#mini-edit-cart').on('click',function(event) {
+    $(this).closest('.container').addClass('edit');
+    event.stopPropagation();
+  });
+  $('#mini-view-cart').on('click',function(event) {
+    $(this).closest('.container').removeClass('edit');
+    event.stopPropagation();
+  });
+
+  $('.secondary-level .tab').on('click',function(event){
+    if ($(this).find('.menu').size() > 0) {
+      $(this).find('.menu').addClass('visible');
+    }
+    event.stopPropagation();
   });
 });
